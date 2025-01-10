f58b7032ec69cb00345a4a74110f65f57035d440
raghuatharva <raghuatharva95@gmail.com>
2025-01-10 21:48:39 +0530

    add

---
+++ datatype.sh
@@ -0,0 +1,8 @@
+num1=10
+num2=12
+
+sum=$((num1+num2))
+echo " the value of $num1 and $num2 is $sum "
+
+
+echo "$HOME" 
---
+++ install.sh
@@ -0,0 +1,21 @@
+userid=$(id -u)
+
+if [ $userid -eq 0 ]; then
+        echo " root access boy , let me check you ve got mysql installed or not "
+        dnf list installed mysql-client
+        else 
+        echo " you dont have root access , BUGGER ;exiting ..."
+        exit 1
+    if [ $? -eq 0 ]; then
+            echo " already installed "
+            else
+            echo " going to install mysql "
+dnf install mysql -y
+        if [ $? -eq 0 ]; then
+            echo " mysql installed SUCCESSFULLY. WELCOME TO DATABASE WORLD"
+            else
+            echo " NOT INSTALLED PROPERLY .. CHECK "
+            exit 1
+        fi
+    fi
+fi
